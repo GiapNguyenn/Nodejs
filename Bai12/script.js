0666d4e157b322f5e07e4a0a577680ac717958dc
GiapNguyenn <truongnguyengiaps@gmail.com>
2025-03-04 11:06:42 +0700

    Update 4/3

--- Bai12/script.js
+++ Bai12/script.js
@@ -41,21 +41,89 @@
 // })
 
 //ví dụ 3
-const loginSuccess = () => {
-    console.log("Đăng nhập thành công")
-}
-const checkLogin = (data,callback) => {
-    const email = "truongnguyengiaps@gmail.com"
-    const password = "123455"
-    if(data.email === email && data.password === password){
-        callback()
-    }else {
-        console.log("Đăng nhập thất bại")
-    }
-}
-let data = {
-    email : "truongnguyengiaps@gmail.com",
-    password : "123455"
-}
-checkLogin(data,loginSuccess)
-//45p05s
+// const loginSuccess = () => {
+//     console.log("Đăng nhập thành công")
+// }
+// const checkLogin = (data,callback) => {
+//     const email = "truongnguyengiaps@gmail.com"
+//     const password = "123455"
+//     if(data.email === email && data.password === password){
+//         callback()
+//     }else {
+//         console.log("Đăng nhập thất bại")
+//     }
+// }
+// let data = {
+//     email : "truongnguyengiaps@gmail.com",
+//     password : "123455"
+// }
+// checkLogin(data,loginSuccess)
+
+// 2
+// var a =10 ;
+// var promise = new Promise ((resolve,reject) => {
+//     if(a ===undefined){
+//         reject()
+//     }else {
+//         resolve(a);
+//     }
+// });
+// promise
+//     .then((a)=>{
+//         console.log(a)
+//         return a
+//     })
+//     .then((a)=>{
+//         const b = a + 10;
+//         console.log(b)
+//         return b
+//     })
+//     .then((b)=>{
+//         const c = b * 20;
+//         console.log(c)
+//     })
+//     .catch(()=>{
+//         console.log("Thất bại")
+//     })
+    // .finally(()=>{
+    //     console.log("Luôn chạy vào đây")
+    // })
+
+    // 3 trang thai 
+    // const promise = new Promise((resolve,reject) =>{
+    //     setTimeout(()=>{
+    //         // resolve(10);
+    //         reject("Loi")
+    //     },3000)
+    // });
+
+    // setTimeout(()=>{
+    //     console.log("Sau 1 giay: ",promise);
+    // },1000)
+    // setTimeout(()=>{
+    //     console.log("Sau 2 giay: ",promise);
+    // },2000)
+    // setTimeout(()=>{
+    //     console.log("Sau 3 giay: ",promise);
+    // },3000)
+//Jsonviewer
+//4
+fetch("https://dummyjson.com/products")
+    .then(response => response.json())
+    .then((data) =>{
+        // console.log(data.products)
+
+    const newArray = data.products.map((item) => {
+        return `
+        <div class="product-item">
+            <img src="${item.thumbnail}">
+            <h2>${item.title}</h2>
+            <h3>${item.price}$</h3>
+        </div>
+        `
+    })
+    const htmls = newArray.join("")
+
+    const productList = document.querySelector("#product-list")
+    productList.innerHTML = htmls
+    })
